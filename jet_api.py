0e53403bbd43d17d68984494abc4209fd95b0731
Miklos Vecsei <vecseimiklos@gmail.com>
2019-12-03 17:54:59 +0000

    added the option of defining teams for uid

--- jet_api.py
+++ jet_api.py
@@ -48,21 +48,38 @@ def getsignal(exp_id, source, no_data = False, options={}):
         return flap.load(filename)
 
     # obtaining the data from the server
+    # if the UID ends with -team, then it looks in the config file, whether
+    # there is a team defined under the name given in UID in the config file
+    # if so, it will loop over the name of the team members
+    split_uid = options["UID"].split("-")
+    if split_uid[-1] == "team":
+        uid_list = flap.config.get("Module JET_API",options["UID"], evaluate=True)
+    else:
+        if options["UID"] == "curr_user":
+            options["UID"] = pwd.getpwuid(os.getuid()).pw_name
+        uid_list = [options["UID"]]
+
     if split_source[0].upper() == "PPF":
-        raw_ppf = \
-                ppf.ppfdata(exp_id,split_source[2],split_source[1],
-                            seq=options["Sequence"], uid=options["UID"],
-                            fix0=options["fix0"], reshape=options["reshape"],
-                            no_x=options["no_x"], no_t=options["no_t"],
-                            no_data=options["Only Info"])
-        [data,x,t,nd,nx,nt,dunits,xunits,tunits,desc,comm,seq,ier] = raw_ppf
+        for uid in uid_list:
+            raw_ppf = \
+                    ppf.ppfdata(exp_id,split_source[2],split_source[1],
+                                seq=options["Sequence"], uid=uid,
+                                fix0=options["fix0"], reshape=options["reshape"],
+                                no_x=options["no_x"], no_t=options["no_t"],
+                                no_data=options["Only Info"])
+            [data,x,t,nd,nx,nt,dunits,xunits,tunits,desc,comm,seq,ier] = raw_ppf
+            if ier == 0:
+                break
         if ier != 0:
             raise ValueError("Error reading "+source+" for shot "+str(exp_id)+
                              ", UID "+options["UID"]+":\nIER "+str(ier)+": "+desc)
     elif split_source[0].upper() == "JPF":
-        raw_jpf = jpf.getdat("/".join(split_source[1:]),
-                             exp_id, nwds = options["nwds"])
-        [data, t, nwds, desc, dunits, ier] = raw_jpf
+        for uid in uid_list:
+            raw_jpf = jpf.getdat("/".join(split_source[1:]),
+                                 exp_id, nwds = options["nwds"])
+            [data, t, nwds, desc, dunits, ier] = raw_jpf
+            if ier == 0:
+                break
         options["no_x"] = 0
         x=[]
         tunits = ""
@@ -74,7 +91,7 @@ def getsignal(exp_id, source, no_data = False, options={}):
     
     # converting data to flap DataObject
     coordinates = []
-    info = "Obtained at "+str(date.today())+"\n"
+    info = "Obtained at "+str(date.today())+", uid "+uid+"\n"
     if options["no_t"] == 0 and (len(t) != 0):
         tunit = ["secs", "s", "seconds"]
         if tunits.lower() in tunit:
@@ -100,6 +117,8 @@ def getsignal(exp_id, source, no_data = False, options={}):
                                          dimension_list=[0])
         coordinates.append(time_coord)
     if options["no_x"] == 0 and (len(x) != 0):
+        if len(x)==1 and len(data)>1:
+            x = np.ones(len(data))*x
         x_coord = flap.Coordinate(name='X', unit=xunits, values=x,
                                      shape=x.shape,
                                      mode=flap.CoordinateMode(equidistant=False),
@@ -135,6 +154,8 @@ def get_data(exp_id=None, data_name=None, no_data=False, options={}, coordinates
          Sequence - the sequence number, if undefined, than the latest data for a pulse is obtained
          UID - the user id under which the ppf is to be found. By default set to jetppf
                if the value is set to "curr_user", than the UID is set to the current user's ID
+               UID can also beam a "-team" in this case it has to be defined in the flap_defaults.cfg
+               under the JET_API Module
          no_x, no_t, Only Info - If no_x and/or no_t is set, then the corresponding
                                coordinate is not returned. If "Only Info" is
                                set then both x, t and the data is not returned,
@@ -157,9 +178,6 @@ def get_data(exp_id=None, data_name=None, no_data=False, options={}, coordinates
             "reshape": 0,
             "nwds": 0}
     options = {**options_default, **options}
-    
-    if options["UID"] == "curr_user":
-        options["UID"] = pwd.getpwuid(os.getuid()).pw_name
         
     d = getsignal(exp_id, data_name, no_data=no_data, options=options)
     
