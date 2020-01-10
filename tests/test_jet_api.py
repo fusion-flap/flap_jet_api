2ea43b5124922b06bac99109f8a1eb311f15f984
Miklos Vecsei <vecseimiklos@gmail.com>
2020-01-10 16:55:42 +0100

    some bugfixes, seems OK

--- tests/test_jet_api.py
+++ tests/test_jet_api.py
@@ -10,18 +10,20 @@ import flap_jet_api as jetapi
 def test_ppf():
     ky6 = jetapi.getsignal(95531, 'JPF/DH/Y6-EMITER<VLT', options={"Check Time Equidistant": True,
                                                                    "Datapath": "PPF",
-                                                                   "Cache Data": True})
+                                                                   "Cache Data": False})
     return int(np.mean(ky6.data)), ky6.get_coordinate_object("Time").mode.equidistant
 
 def test_sal():
     ky6 = jetapi.getsignal(95531, 'JPF/DH/Y6-EMITER<VLT', options={"Check Time Equidistant": True,
                                                                    "Datapath": "SAL",
-                                                                   "Cache Data": True})
+                                                                   "Cache Data": False})
     return int(np.mean(ky6.data)), ky6.get_coordinate_object("Time").mode.equidistant
 
 def test_cache():
     cached_file = os.sep.join(jetapi.__file__.split(os.sep)[:-1] + ['cached','ppf_ky6i_cali-mvecsei-95531.hdf5'])
     existed = os.path.exists(cached_file)
+    if existed is True:
+        print("Can't properly test data caching, file already exists")
     try:
         jetapi.getsignal(95531, 'ppf/ky6i/cali', options={"Check Time Equidistant": True,
                                                           "Datapath": "SAL",
@@ -76,9 +78,9 @@ class StandaloneTest(unittest.TestCase):
     def test_cache(self):
         self.assertTrue(test_cache())
     def test_team(self):
-        self.assertTrue(test_cache())
+        self.assertTrue(test_team())
     def test_signal_list(self):
-        self.assertTrue(test_cache())
+        self.assertTrue(test_signal_list())
 
 # -------------------------------------COMPATIBILITY WITH FLAP----------------------------------------------------------
 
