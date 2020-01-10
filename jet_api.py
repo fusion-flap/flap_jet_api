4a0c1a4686ae06a4335f4adf1b18697b348acf9c
Miklos Vecsei <vecseimiklos@gmail.com>
2020-01-10 11:19:16 +0100

    added SAL access option

--- jet_api.py
+++ jet_api.py
@@ -9,8 +9,7 @@
 
 
 """
-import ppf
-import getdat as jpf
+
 import os
 import pwd
 import flap
@@ -18,11 +17,23 @@ import numpy as np
 from datetime import date
 
 def getsignal(exp_id, source, no_data = False, options={}):
-    """ Signal reading function for the JET API
+    options_default = {"datapath": flap.config.get("Module JET_API","Datapath")}
+    options = {**options_default, **options}
+    if options["datapath"].upper() == 'SAL':
+        d = getsignal_sal(exp_id, source, no_data=no_data, options=options)
+    elif options["datapath"].upper() == 'PPF':
+        d = getsignal_ppf(exp_id, source, no_data=no_data, options=options)
+
+    return d
+
+def getsignal_ppf(exp_id, source, no_data = False, options={}):
+    """ Signal reading function for the JET API PPF
     Does not require using the FLAP storage
     Input: same as get_data()
     Output: DataObject for the given source
     """
+    import ppf
+    import getdat as jpf
     # Function used for getting either ppf or jpf data from the server
     # 
     options_default = {"Sequence": 0,
@@ -142,6 +153,126 @@ def getsignal(exp_id, source, no_data = False, options={}):
     else:
         # if one is only ineterested in the metadata
         return raw_ppf
+    
+def getsignal_sal(exp_id, source, no_data = False, options={}):
+    """ Signal reading function for the JET API SAL
+    Does not require using the FLAP storage
+    Input: same as get_data()
+    Output: DataObject for the given source
+    """
+    # Function used for getting either ppf or jpf data from the server
+    #
+    from jet.data import sal
+    
+    options_default = {"Sequence": 0,
+                       "UID": "jetppf",
+                       "Check Time Equidistant": True,
+                       "Cache Data": True}
+    options = {**options_default, **options}
+    
+    #checking if the data is already in the cache
+    curr_path = os.path.dirname(os.path.abspath(__file__))
+    location = os.path.sep.join(curr_path.split(os.path.sep))
+    split_source = source.split("/")
+    filename = os.path.join(os.path.sep.join([location,"cached"]),
+                            ("_".join(split_source)+"-"+options["UID"]+\
+                            "-"+str(exp_id)+".hdf5").lower())
+    if os.path.exists(filename):
+        return flap.load(filename)
+
+    # obtaining the data from the server
+    # if the UID ends with -team, then it looks in the config file, whether
+    # there is a team defined under the name given in UID in the config file
+    # if so, it will loop over the name of the team members
+    split_uid = options["UID"].split("-")
+    if split_uid[-1] == "team":
+        uid_list = flap.config.get("Module JET_API",options["UID"], evaluate=True)
+    else:
+        if options["UID"] == "curr_user":
+            options["UID"] = pwd.getpwuid(os.getuid()).pw_name
+        uid_list = [options["UID"]]
+
+    # has to do some character conversion in the names as SAL uses different
+    # naming as the standard ppf names
+    character_dict = {'>':'_out_',
+                      '<':'_in_',
+                      ':':'_sq_',
+                      '$':'_do_',
+                      '&':'_et_',
+                      ' ':'_sp_'}
+    node = split_source[2].lower()
+    for char in character_dict.keys():
+        node = character_dict[char].join(node.split(char))
+
+    signal_error = None
+    error_string = str()
+    for uid in uid_list:
+        try:
+            if split_source[0].lower() == "ppf":
+                source_string = "/pulse/"+str(exp_id)+"/"+split_source[0].lower()+"/signal/"+uid.lower()+"/"+split_source[1].lower()\
+                            +"/"+node
+            elif split_source[0].lower() == "jpf":
+                source_string = "/pulse/"+str(exp_id)+"/"+split_source[0].lower()+"/"+split_source[1].lower()\
+                            +"/"+node+"/data"
+            else:
+                raise ValueError("Source should be either jpf or ppf")
+            if options["Sequence"] != 0:
+                source_string = source_string + ":"+str(options["Sequence"])
+            raw_signal = sal.get(source_string)
+            data = raw_signal.data
+            signal_error = raw_signal.error
+        except sal.SALException as e:
+            error_string = error_string + source_string + " " + str(e) +"\n"
+    if not (signal_error is None):
+        raise ValueError("Error reading "+source+" for shot "+str(exp_id)+
+                         ", UID "+options["UID"]+":\nIER "+str(ier)+": "+desc)
+    elif not ("data" in locals()):
+        raise ValueError("No data found with errors: \n"+error_string)
+    # converting data to flap DataObject
+    coordinates = []
+    data = data.reshape(raw_signal.shape)
+    info = "Obtained at "+str(date.today())+", uid "+uid+"\n"
+    coord_dimension = 0
+    for coord in raw_signal.dimensions:
+        name =  coord.description
+        values = np.array(coord.data)
+        unit = coord.units
+        equidist = False
+        if name.lower()=='time' or name.lower()=='t' or coord.temporal is True:
+            tunit = ["secs", "s", "seconds"]
+            if unit.lower() in tunit:
+                unit = "Second"
+            equidist = False
+            if options['Check Time Equidistant'] is True and (unit == "Second" or coord.temporal is True):
+                timesteps = np.array(values[1:])-np.array(values[0:-1])
+                equidistance = np.linalg.norm(timesteps-timesteps[0])/np.linalg.norm(timesteps)
+                if equidistance < 1e-6:
+                    info = info + "Time variable is taken as equidistant to an accuracy of "+\
+                           str(equidistance)+"\n"
+                    coord_object = flap.Coordinate(name='Time', unit=values, start = t[0],
+                                                   shape=values.shape, step=np.mean(timesteps),
+                                                   mode=flap.CoordinateMode(equidistant=True),
+                                                   dimension_list=[0])
+                    equidist = True
+        if equidist is False:
+            coord_object = flap.Coordinate(name=name, unit=unit, values=values,
+                                           shape=values.shape,
+                                           mode=flap.CoordinateMode(equidistant=False),
+                                           dimension_list=[coord_dimension])
+        coordinates.append(coord_object)
+        coord_dimension = coord_dimension+1
+
+    unit = flap.Unit(name=source,unit=raw_signal.units)
+    signal = flap.DataObject(data_array=data, data_unit=unit,
+                             coordinates=coordinates, exp_id=str(exp_id),
+                             data_title=raw_signal.description, data_shape=data.shape,
+                             info=info)
+    if "summary" in dir(raw_signal):
+        signal.info = signal.info+raw_signal.summary().description+"\n"
+    if options["Cache Data"] is True:
+        flap.save(signal, filename)
+    return signal
+
 
 def get_data(exp_id=None, data_name=None, no_data=False, options={}, coordinates=None, data_source=None):
     """ Signal reading function for the JET API
@@ -177,11 +308,12 @@ def get_data(exp_id=None, data_name=None, no_data=False, options={}, coordinates
             "Cache Data": False,
             "fix0": 0,
             "reshape": 0,
-            "nwds": 0}
+            "nwds": 0,
+            "datapath": flap.config.get("Module JET_API","Datapath")}
     options = {**options_default, **options}
-        
-    d = getsignal(exp_id, data_name, no_data=no_data, options=options)
     
+    d = getsignal(exp_id, data_name, no_data=no_data, options=options)
+
     return d
 
 def add_coordinate(data_object, new_coordinates, options={}):
